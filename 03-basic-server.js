e55907569d050e9a73b2dfc222d2a35cec1808cc
unknown <gabrielantalsilva@gmail.com>
2021-04-13 22:08:57 -0300

    feito a primeira atividade

--- 03-basic-server.js
+++ 03-basic-server.js
@@ -3,20 +3,25 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    
 
+    
+ app.get('/', (req, res) => {
+    
+    (async ()=>{
     // leitura async libera o event loop para processar outras requisicoes
-    fs.readFile('./html/basic.html', 'utf-8', (err, data) => {
-        if (err) {
-            res.send(err);
-            return;
-        } 
-        res.send(data);
-    });
+        await fs.readFile('./html/basic.html', 'utf-8', (err, data) => {
+            if (err) {
+                res.send(err);
+                return;
+            } 
+            res.send(data);
+        });
+    })();
 
     console.log('leitura do arquivo ');
 });
+
+
 
 app.listen(3000, () => {
     console.log(`Example app listening at http://localhost:3000`);
